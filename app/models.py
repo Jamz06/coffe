92b2251fc70a710b0bf37a38cb81d5611f27ba19
Lihoded <jamshaha@mail.ru>
2018-08-04 11:21:18 +0500

    Создан механизм входа в систему

--- app/models.py
+++ app/models.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db
+# UserMixin, добавляет методы, которые использует FlaskLogin. Можно рассписать из руками, но можно и просто добавить готовые.
+from flask_login import UserMixin
 
-class User(db.Model):
+from app import db, login
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,47 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+
+from werkzeug.urls import url_parse
 
 from app import app
 
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
     
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Юзер {}, remember {}'.format(
-            form.username.data,
-            form.remember_me.data
-        ))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
 
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Неверный логин или пароль')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+
+        # В адресе, может быть ссылка на запрошенную страницу
+        # Парсим, и пишем в переменную next_page
+        next_page = request.args.get('next')
+        # Если нет аргумента next, или он содержит полный URL, с указанием домена
+        # то перенаправляем на index (мало-ли чего мне в next вставят:) )
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
+
     return render_template('login.html', title='Вход', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
