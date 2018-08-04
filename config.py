6f4c9bb9f8b41d5bfe7d3c642b625bb8e987e78c
Lihoded <jamshaha@mail.ru>
2018-08-04 09:25:43 +0500

    Создан сценарий миграции и произведено создание таблицы Users

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ASDNL<K#JUO@o2i3u10lsadh;lsa213dfJASID932'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'coffe.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
