a0d1baa87851bda322c020295a95e88473f00c9f
Lihoded <jamshaha@mail.ru>
2018-08-04 10:08:54 +0500

    Добавлены операции и клиенты

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -8,3 +9,23 @@ class User(db.Model):
     
     def __repr__(self):
         return 'Пользователь {}'.format(self.name)
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(11), index=True, unique=True)
+    comment = db.Column(db.String(64))
+    operations = db.relationship('Operation', backref='operation', lazy='dynamic' )
+    def __repr__(self):
+        return 'Клиент {}'.format(self.phone)
+
+class Operation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    vid_op = db.Column(db.Integer, db.ForeignKey('vid_op.id'))
+    sum = db.Column(db.Float)
+    bonus = db.Column(db.Float)
+
+class Vid_op(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nazv = db.Column(db.String(28))
