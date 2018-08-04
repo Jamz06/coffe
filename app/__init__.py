ff8df2956eb33ec2fd205b91277234e7f26c7a3d
Lihoded <jamshaha@mail.ru>
2018-08-04 12:04:36 +0500

    Прежде чем вводить новые стили

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,26 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
+# Инициализация flask
 app = Flask(__name__)
+# Подключение конфигурций из config.py
 app.config.from_object(Config)
+
+# Создание экземпляра SQLAlchemy для ORM
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Инициализация расширения LoginManager
 login = LoginManager(app)
+# Указание на функцию, куда отправлять незарегистрированных
 login.login_view = 'login'
 
+# Инициализация расширения Bootstrap
+bootstrap = Bootstrap(app)
 
+# Подключение остальных
 from app import routes, models
 
 
